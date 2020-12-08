a7a4a49b15db29ef7104c4e7eafb5430eda4986b
Pernilla Sterner <psterner@hotmail.se>
2020-12-08 20:08:24 +0100

    Create Hero Banner Custom Field  & Add Hero Banner Section to Startpage

--- front-page.php
+++ front-page.php
@@ -1,8 +1,13 @@
 <?php get_header(); ?>
 
-  <?php while(have_posts()) : ?>
-    <?php the_post(); ?>
-    <!-- ADD ACF -->
-  <?php endwhile; ?>
+  <?php while(have_posts()) : the_post();
+    
+    /**______HERO BANNER SECTION______**/
+    get_template_part( '/temp/hero-banner' );
+    
+    
+   
+
+  endwhile;?>
 
 <?php get_footer(); ?>
