97b56143be4a184b031930b67014178e90ecf226
Pernilla Sterner <psterner@hotmail.se>
2020-12-11 22:55:17 +0100

    Add temporary code for review section

---
+++ template-parts/review.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Section Review
+ */
+?>
+
+<!-- TODO: GET THE API -->
+
+<div class="review-container">
+
+  <?php for($i = 1; $i <= 3; $i++) { ?>
+
+    <div class="review-container">
+      <div class="review box_$i">
+        <p class="comment"><strong>Du är så jävla fantastisk, du och dina pass är min höjdpunkt på hela veckan! Dina pass väcker ett enormt driv i mig, dels i yogan men också som medmänniska! Så tack för att du är du!🌸</strong></p>
+        <p class="comment_by"><strong>Paula</strong></p>
+      </div>
+    </div>
+
+  <?php } ?>
+  
+</div>
