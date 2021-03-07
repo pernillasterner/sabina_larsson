bc77a99d130ac1116a439f52602edf8d76d12b06
Pernilla Sterner <psterner@hotmail.se>
2021-03-07 13:39:35 +0100

    Add more workshop-blocks to startpage

--- template-parts/workshop.php
+++ template-parts/workshop.php
@@ -9,48 +9,62 @@
 $args = array(
   'post_type' => 'workshop',
   'post_status' => 'publish',
-  'posts_per_page' => 1
+  'posts_per_page' => 4
 );
 $workshops = new WP_Query($args); ?>
 
-<div id="<?php echo 'workshop'; ?>" class="site-workshop-content"> 
+<div id="<?php echo 'workshop'; ?>" class="site-workshop-content workshop-sabina"> 
 
   <?php while( $workshops->have_posts() ) : $workshops->the_post(); ?>
   
     <?php 
     $image50 = get_field('image_50');
     $content = get_field('content_1');
+    $heading = [];
+    
+      for ($i = 0; $i <= 3; $i++) {
+        $content_big = get_field('content_' . $i);
+        if (!empty($content_big['heading']) && $content_big['heading']) {
+          array_push($heading, $content_big['heading']);
+         
+          if (count($heading) < 3) {
+            $className =  "image-container";
+          } else {
+            $className = 'image-container-big';
+          } 
+        }
+      }
     ?>
-
-    <div class="image-container" style="background-image: url(<?php echo $image50['url']; ?>)"></div>
+    <div class="courses-wrapper">
+      <div class="<?php echo $className; ?>" style="background-image: url(<?php echo $image50['url']; ?>)"></div>
       <div class="content-container">
         <div class="content-inner-container">
-    
-          <div class="title-container">
-            <h2 class="title"><?php echo $content['title']; ?></h2>
-            <p class="date"><?php  echo $content['date']; ?></p>
-          </div>
+  
+        <div class="title-container">
+          <h2 class="title"><?php echo $content['title']; ?></h2>
+          <p class="date"><?php  echo $content['date']; ?></p>
+        </div>
 
-          <?php
-          // Loop through all custom fields
-            for ($i = 1; $i <= 2; $i++) {
-            
-              $content = get_field('content_' . $i); ?>
-            
-              <div class="info-container">
-                <h3 class="heading"><?php  echo $content['heading']; ?></h3>
-                <h4 class="sub-heading"><?php  echo $content['sub_heading']; ?></h4>
-                <p class="time"><?php  echo $content['time']; ?></p>
-                <div class="textarea">
-                  <p class="text"><?php  echo $content['text']; ?></p>
-                </div>
-                <h6 class="price">Pris: <?php  echo $content['price']; ?>kr</h6>
+        <?php
+        // Loop through all custom fields
+          for ($i = 1; $i <= 3; $i++) {
+          
+            $content = get_field('content_' . $i); ?>
+          
+            <div class="info-container">
+              <h3 class="heading"><?php  echo $content['heading']; ?></h3>
+              <h4 class="sub-heading"><?php  echo $content['sub_heading']; ?></h4>
+              <p class="time"><?php  echo $content['time']; ?></p>
+              <div class="textarea">
+                <p class="text"><?php  echo $content['text']; ?></p>
               </div>
-            <?php } ?>
-        
+              <h6 class="price">Pris: <?php  echo $content['price']; ?>kr</h6>
+            </div>
+          <?php } ?>
+      
 
-          <div class="notification"><strong>Betalning sker vid anmälan.</strong><a class="mailto" href = "mailto: sabinalarssonyoga@outlook.com">sabinalarssonyoga@outlook.com</a></div>
-        
+        <div class="notification"><strong>Betalning sker vid anmälan.</strong><a class="mailto" href = "mailto: sabinalarssonyoga@outlook.com">sabinalarssonyoga@outlook.com</a></div>
+      
         </div>
 
         <div class="general-info">
@@ -66,6 +80,7 @@ $workshops = new WP_Query($args); ?>
           </div>
         </div>
       </div>
+    </div>
 
   <?php endwhile; ?>
 </div>
