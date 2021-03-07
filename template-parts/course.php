0a98a8468785a80e77b4278646dcf589688c9d9b
Pernilla Sterner <psterner@hotmail.se>
2021-03-07 13:11:15 +0100

    Add more course blocks to startpage

--- template-parts/course.php
+++ template-parts/course.php
@@ -9,60 +9,79 @@
 $args = array(
   'post_type' => 'course',
   'post_status' => 'publish',
-  'posts_per_page' => 1
+  'posts_per_page' => 3
 );
 $courses = new WP_Query($args);
 ?>
 
-<div id="courses" class="site-workshop-content"> 
+<div id="courses" class="site-courses-content"> 
 
   <?php while( $courses->have_posts() ) : $courses->the_post(); ?>
   
     <?php 
     $image50 = get_field('image_50');
     $content = get_field('content_1');
+    $heading = [];
+    
+      for ($i = 0; $i <= 3; $i++) {
+        $content_big = get_field('content_' . $i);
+        if (!empty($content_big['heading']) && $content_big['heading']) {
+          array_push($heading, $content_big['heading']);
+         
+          if (count($heading) < 3) {
+            $className =  "image-container";
+          } else {
+            $className = 'image-container-big';
+          } 
+        }
+      }
     ?>
 
-    <div class="image-container" style="background-image: url(<?php echo $image50['url']; ?>)"></div>
-      <div class="content-container">
-        <div class="content-inner-container">
-    
-          <div class="title-container">
-            <h2 class="title"><?php echo $content['title']; ?></h2>
-            <p class="date"><?php  echo $content['date']; ?></p>
-          </div>
+    <div class="courses-wrapper">
+      <div class="<?php echo $className; ?>" style="background-image: url(<?php echo $image50['url']; ?>)"></div>
+        <div class="content-container">
+          <div class="content-inner-container">
+      
+            <div class="title-container">
+              <h2 class="title"><?php echo $content['title']; ?></h2>
+              <p class="date"><?php  echo $content['date']; ?></p>
+            </div>
 
-          <?php
-          // Loop through all custom fields
-            for ($i = 1; $i <= 2; $i++) {
+            <?php
+            // Loop through all custom fields
               
-              $content = get_field('content_' . $i); ?>
-            
-              <div class="info-container">
-                <h3 class="heading"><?php  echo $content['heading']; ?></h3>
-                <h4 class="sub-heading"><?php  echo $content['sub_heading']; ?></h4>
-                <p class="time"><?php  echo $content['time']; ?></p>
-                <div class="textarea">
-                  <p class="text"><?php  echo $content['text']; ?></p>
+              for ($i = 1; $i <= 3; $i++) {
+                
+                $content = get_field('content_' . $i); 
+                
+                ?>
+              
+                <div class="info-container">
+                  <h3 class="heading"><?php  echo $content['heading']; ?></h3>
+                  <h4 class="sub-heading"><?php  echo $content['sub_heading']; ?></h4>
+                  <p class="time"><?php  echo $content['time']; ?></p>
+                  <div class="textarea">
+                    <p class="text"><?php  echo $content['text']; ?></p>
+                  </div>
+                  <h6 class="price">Pris: <?php  echo $content['price']; ?></h6>
                 </div>
-                <h6 class="price">Pris: <?php  echo $content['price']; ?></h6>
-              </div>
-            <?php } ?>
-  
-          <div class="notification"><strong>Betalning sker vid anmälan.</strong><a class="mailto" href = "mailto: sabinalarssonyoga@outlook.com">sabinalarssonyoga@outlook.com</a></div>
-        
-        </div>
+              <?php } ?>
+    
+            <div class="notification"><strong>Betalning sker vid anmälan.</strong><a class="mailto" href = "mailto: sabinalarssonyoga@outlook.com">sabinalarssonyoga@outlook.com</a></div>
 
-        <div class="general-info">
-          <div class="location">
-            <h3>PLATS</h3>
-            <p class="place"><?php echo $content['place']; ?></p>
-            <p class="adress"><?php echo $content['adress']; ?></p>
           </div>
-          <div class="payment">
-            <h3>BETALNING</h3>
-            <p class="swish">Swish: 123 190 01 82</p>
-            <p class="bankgiro">Bankgiro: 5529-6065</p>
+
+          <div class="general-info">
+            <div class="location">
+              <h3>PLATS</h3>
+              <p class="place"><?php echo $content['place']; ?></p>
+              <p class="adress"><?php echo $content['adress']; ?></p>
+            </div>
+            <div class="payment">
+              <h3>BETALNING</h3>
+              <p class="swish">Swish: 123 190 01 82</p>
+              <p class="bankgiro">Bankgiro: 5529-6065</p>
+            </div>
           </div>
         </div>
       </div>
